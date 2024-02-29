f5a4b1897a352a1e259f6476a995fe0c958bfee2
Erick El nino <erick.tshimbombo@yahoo.com>
2024-02-29 05:13:34 +0200

    added aall database variable

--- db/Database.js
+++ db/Database.js
@@ -1,12 +1,15 @@
 import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "erickelnino",
-    password: "",
-    database: "smart_brain",
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
 });
 
