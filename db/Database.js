de2d6499099089b0e7d69725fa3ad8fddb330d43
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-03 16:52:18 +0200

    update hotfix

--- db/Database.js
+++ db/Database.js
@@ -6,12 +6,27 @@ dotenv.config();
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host:
+      process.env.HOST ||
+      "dpg-cnftquta73kc73dcofeg-a.oregon-postgres.render.com",
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER || "erickelnino",
+    password: process.env.DB_PASS || "IPLXRbudYD3h9cTu6YRlt4c3jX7aJchV",
+    database: process.env.DB_NAME || "smart_brain_ftau",
     ssl: true,
   },
 });
 
 export default db;
+
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     host: "dpg-cma6u2md3nmc73cmvtbg-a.oregon-postgres.render.com",
+//     port: 5432,
+//     user: "find_a_face_admin",
+//     password: "rmyOI6ZdzKglnG0nWJh91OU1MV6GbqKN",
+//     database: "find_a_face_db",
+//     ssl: true,
+//   },
+// });
